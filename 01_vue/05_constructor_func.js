f62f9ac9c8f9514188340a04b6174b172f8782a6
yooooonzzzzzang <cherishvalue@naver.com>
2022-11-01 15:54:14 +0900

    Add 02_vue

--- 01_vue/05_constructor_func.js
+++ 01_vue/05_constructor_func.js
@@ -9,11 +9,11 @@ const member2 = {
   age: 20,
   sId: 2022311492,
 }
-
+// 생성자 함수, 함수이름 대문자로 시작
 function Member(name, age, sId) {
   this.name = name
   this.age = age
   this.sId = sId
 }
-
+// 생성자 함수를 사용할때 반드시 new 연산자 사용
 const member3 = new Member('isaac', 21, 2022654321)
