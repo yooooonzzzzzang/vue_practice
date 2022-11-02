175e4fa2deaa83e22e622dd9f64abc7d1d4bbfde
yooooonzzzzzang <cherishvalue@naver.com>
2022-11-02 23:22:07 +0900

    Add: q1

---
+++ 03_vue/q1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>이곳은 App.vue입니다.</h1>
+    <HelloWorld msg="SSAFY 여러분을 응원합니다!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
